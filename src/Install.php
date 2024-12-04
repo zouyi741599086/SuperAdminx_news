0565797b90a17acdd4a48e67234d6fcce92e9048
邹意 <741599086@qq.com>
2024-12-04 20:44:06 +0800

    Initial commit

--- src/Install.php
+++ src/Install.php
@@ -203,10 +203,6 @@ class Install
      */
     private static function installSql($sqlPath)
     {
-        echo config('thinkorm.default');
-        echo '换行\n';
-        echo getenv('DB_HOST');
-        echo '换行\n';
         // 配置
         Db::setConfig([
             // 默认数据连接标识
@@ -217,15 +213,15 @@ class Install
                     // 数据库类型
                     'type'            => 'mysql',
                     // 服务器地址
-                    'hostname'        => getenv('DB_HOST'),
+                    'hostname'        => '192.168.1.168',
                     // 数据库名
-                    'database'        => getenv('DB_NAME'),
+                    'database'        => 'superadminx',
                     // 数据库用户名
-                    'username'        => getenv('DB_USER'),
+                    'username'        => 'root',
                     // 数据库密码
-                    'password'        => getenv('DB_PASSWORD'),
+                    'password'        => 'root',
                     // 数据库连接端口
-                    'hostport'        => getenv('DB_PORT'),
+                    'hostport'        => 3306,
                     // 数据库连接参数
                     'params'          => [
                         // 连接超时3秒
@@ -234,7 +230,7 @@ class Install
                     // 数据库编码默认采用utf8
                     'charset'         => 'utf8mb4',
                     // 数据库表前缀
-                    'prefix'          => getenv('DB_PREFIX'),
+                    'prefix'          => 'sa_',
                     // 断线重连
                     'break_reconnect' => true,
                     // 关闭SQL监听日志
