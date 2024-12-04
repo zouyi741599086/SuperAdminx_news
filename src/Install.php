108f734e35b4d5bd346d6e517d9dd075fafdd566
邹意 <741599086@qq.com>
2024-12-04 20:27:25 +0800

    Initial commit

--- src/Install.php
+++ src/Install.php
@@ -203,10 +203,36 @@ class Install
      */
     private static function installSql($sqlPath)
     {
-        $config = config('thinkorm');
-        var_dump($config);
         // 配置
-        Db::setConfig($config);
+        Db::setConfig([
+            // 默认数据连接标识
+            'default'     => 'mysql',
+            // 数据库连接信息
+            'connections' => [
+                'mysql' => [
+                    // 数据库类型
+                    'type'            => 'mysql',
+                    // 服务器地址
+                    'hostname'        => getenv('DB_HOST'),
+                    // 数据库名
+                    'database'        => getenv('DB_NAME'),
+                    // 数据库用户名
+                    'username'        => getenv('DB_USER'),
+                    // 数据库密码
+                    'password'        => getenv('DB_PASSWORD'),
+                    // 数据库连接端口
+                    'hostport'        => getenv('DB_PORT'),
+                    // 数据库编码默认采用utf8
+                    'charset'         => 'utf8',
+                    // 数据库表前缀
+                    'prefix'          => getenv('DB_PREFIX'),
+                    // 数据库调试模式
+                    'debug'           => true,
+                    // 断线重连
+                    'break_reconnect' => true,
+                ],
+            ],
+        ]);
 
         $sqlContent = file_get_contents($sqlPath);
         // 尝试分割 SQL 语句（注意：这只是一个简单的示例，可能不适用于所有情况）
