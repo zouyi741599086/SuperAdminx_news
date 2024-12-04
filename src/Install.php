cf25583f494d95da2d045cdd001e39997bd5784c
邹意 <741599086@qq.com>
2024-12-04 20:30:19 +0800

    Initial commit

--- src/Install.php
+++ src/Install.php
@@ -222,14 +222,25 @@ class Install
                     'password'        => getenv('DB_PASSWORD'),
                     // 数据库连接端口
                     'hostport'        => getenv('DB_PORT'),
+                    // 数据库连接参数
+                    'params'          => [
+                        // 连接超时3秒
+                        \PDO::ATTR_TIMEOUT => 3,
+                    ],
                     // 数据库编码默认采用utf8
-                    'charset'         => 'utf8',
+                    'charset'         => 'utf8mb4',
                     // 数据库表前缀
                     'prefix'          => getenv('DB_PREFIX'),
-                    // 数据库调试模式
-                    'debug'           => true,
                     // 断线重连
                     'break_reconnect' => true,
+                    // 关闭SQL监听日志
+                    'trigger_sql'     => false,
+                    // 自定义分页类
+                    'bootstrap'       => '',
+                    // 是否严格检查字段是否存在
+                    'fields_strict'   => false,
+                    // 开启字段缓存
+                    'fields_cache'    => true,
                 ],
             ],
         ]);
