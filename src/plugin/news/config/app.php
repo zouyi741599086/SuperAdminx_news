e1ee07829ba22fcd667c64420f5ad5fda300f134
邹意 <741599086@qq.com>
2025-05-14 11:27:44 +0800

    常规优化

--- src/plugin/news/config/app.php
+++ src/plugin/news/config/app.php
@@ -3,7 +3,7 @@
 use support\Request;
 
 return [
-    'debug' => getenv('DE_BUG') == 'true' ? true : false,,
+    'debug' => getenv('DE_BUG') == 'true' ? true : false,
     'controller_suffix' => '',
     'controller_reuse' => true,
     'version' => '1.1.3'
